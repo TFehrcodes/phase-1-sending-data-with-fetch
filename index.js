0f5be08ff59580e747d33b59714e348616ac7839
Travis Fehr <travisefehr@gmail.com>
2022-05-23 15:47:20 -0700

    commit tree 255887b2e33a4b8d09a8aeabd652b4ca2bda954b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body:JSON.stringify({
+      "name": userName,
+      "email": userEmail
+    })
+  }
+  
+  return fetch('http://localhost:3000/users', configurationObject)
+  .then(function (response) {
+    console.log(response)
+    return response.json();
+  })
+  .then(function (object) {
+    document.querySelector('body').innerHTML+= object.id
+    console.log(object)
+  })
+  .catch(function (error) {
+    document.querySelector('body').innerHTML+= error.message
+  })
+}
